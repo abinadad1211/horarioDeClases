4018bf14bb2af503bb36dfa6ea250534f91624f4
zs18015552 <57199582+zs18015552@users.noreply.github.com>
2020-06-25 19:04:30 -0500

    Create Horario.java

---
+++ HorarioClases/src/horarioclases/Horario.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horarioclases;
+
+/**
+ *
+ * @author Luis
+ */
+public class Horario {
+    protected String folioHorario;
+    protected String grupo;
+    protected String programaEducativo;
+
+    public Horario(String folioHorario, String grupo, String programaEducativo) {
+        this.folioHorario = folioHorario;
+        this.grupo = grupo;
+        this.programaEducativo = programaEducativo;
+    }
+   
+    public String getFolioHorario() {
+        return folioHorario;
+    }
+
+    public void setFolioHorario(String folioHorario) {
+        this.folioHorario = folioHorario;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getProgramaEducativo() {
+        return programaEducativo;
+    }
+
+    public void setProgramaEducativo(String programaEducativo) {
+        this.programaEducativo = programaEducativo;
+    }
+    
+    
+}
